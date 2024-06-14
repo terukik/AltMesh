45b55477e25162ba5f7d144d810b8ce8c24fd702
Teruki Kirihata <teruki.kirihata@gmail.com>
2024-06-15 00:06:26 +0900

    add src/AltMesh.h

---
+++ src/AltMesh.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Teruki Kirihata <teruki.kirihata@gmail.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
